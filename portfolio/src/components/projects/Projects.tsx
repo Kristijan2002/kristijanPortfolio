7edaeb004253f41eb62ab2cdf9e2896dfb4d095f
Kristijan20 <kikomaksimovski2002@gmail.com>
2024-02-15 14:51:05 +0100

    .

--- portfolio/src/components/projects/Projects.tsx
+++ portfolio/src/components/projects/Projects.tsx
@@ -13,19 +13,19 @@ const Projects: React.FC = () => {
                     <img src="/breathe.mk.svg" alt="breathe mk" />
                 </div>
                 <div className={style.desc}>
-                    <p><span>BREATHE MK</span> - Welcome to our upcoming platform designed to empower the residents of Skopje to report instances of pollution in their city. Our website is currently under construction, but we're working diligently to create a seamless experience for you. Your feedback and inquiries are highly valued as we strive to make this platform as effective as possible. For any questions or to provide feedback, please feel free to reach out to Marija Salatic, our PO, at marija@revelapps.com.</p>
+                    {/* <p><b>BREATHE MK</b> - Welcome to our upcoming platform designed to empower the residents of Skopje to report instances of pollution in their city. Our website is currently under construction, but we're working diligently to create a seamless experience for you. Your feedback and inquiries are highly valued as we strive to make this platform as effective as possible. For any questions or to provide feedback, please feel free to reach out to Marija Salatic, our PO, at marija@revelapps.com.</p> */}
                     <p className={style.tehnologies}>
                         Tehnologies: 
                         <img src="/Laravel.svg" alt="laravel icon" /> Laravel
                         <img src="/vscode-icons_file-type-css (1).svg" alt="styles icon" /> SASS
                         <img src="/logos_javascript (1).svg" alt="styles icon" /> JavaScript
-                        </p>
+                    </p>
                 </div>
             </div>
 
             <div className={style.project}>
                 <div className={style.desc}>
-                    <p className={style.alignRight}><span>KRIK WEBSITE</span> - Engineered a cutting-edge web application utilizing Next.js for the renowned non-governmental organization, Crik. This platform optimizes the presentation of the organization's initiatives, fostering international support. While the application is still under development, stay tuned for its public release!</p>
+                    {/* <p className={style.alignRight}><b>KRIK WEBSITE</b> - Engineered a cutting-edge web application utilizing Next.js for the renowned non-governmental organization, Crik. This platform optimizes the presentation of the organization's initiatives, fostering international support. While the application is still under development, stay tuned for its public release!</p> */}
                     <p className={style.tehnologies}>
                         Tehnologies: 
                         <img src="/devicon_nextjs (1).svg" alt="nextjs icon" /> NextJs
@@ -43,7 +43,7 @@ const Projects: React.FC = () => {
                     <img src="/Marinov Design.svg" alt="marinov design" />
                 </div>
                 <div className={style.desc}>
-                    <p><span>MARINOV DESIGN</span> - Engineered an E-Commerce web application tailored for Marinov Design, a thriving business in Ohrid. Leveraging Next.js, the platform amplifies online visibility, ensures secure transactions, and fuels revenue growth.</p>
+                    {/* <p><b>MARINOV DESIGN</b> - Engineered an E-Commerce web application tailored for Marinov Design, a thriving business in Ohrid. Leveraging Next.js, the platform amplifies online visibility, ensures secure transactions, and fuels revenue growth.</p> */}
                     <p className={style.tehnologies}>
                         Tehnologies: 
                         <img src="/devicon_nextjs (1).svg" alt="nextjs icon" /> NextJs
@@ -55,7 +55,7 @@ const Projects: React.FC = () => {
 
             <div className={style.project}>
                 <div className={style.desc}>
-                    <p className={style.alignRight}><span>STREETARTIST</span> - Welcome to our innovative online platform tailored specifically for street artists. Our platform offers a comprehensive solution for managing earnings and provides a dedicated digital canvas to showcase your exceptional artworks to the world. Experience the convenience and power of this unique platform, meticulously designed for seamless use on mobile devices.</p>
+                    <p className={style.alignRight}><b>STREETARTIST</b> - Welcome to our innovative online platform tailored specifically for street artists. Our platform offers a comprehensive solution for managing earnings and provides a dedicated digital canvas to showcase your exceptional artworks to the world. Experience the convenience and power of this unique platform, meticulously designed for seamless use on mobile devices.</p>
                     <p className={style.tehnologies}>
                         Tehnologies: 
                         <img src="/devicon_html5 (1).svg" alt="nextjs icon" /> HTML5
@@ -73,7 +73,7 @@ const Projects: React.FC = () => {
                     <img src="/brainsterlabs.svg" alt="brainster labs" />
                 </div>
                 <div className={style.desc}>
-                    <p><span>BRAINSTERLABS</span> - Discover the Brainster Labs website - an online platform where students from all Brainster academies can proudly showcase their projects. Experience this responsive website, meticulously crafted for seamless viewing on mobile devices, tablets, laptops, and desktop computers.</p>
+                    <p><b>BRAINSTERLABS</b> - Discover the Brainster Labs website - an online platform where students from all Brainster academies can proudly showcase their projects. Experience this responsive website, meticulously crafted for seamless viewing on mobile devices, tablets, laptops, and desktop computers.</p>
                     <p className={style.tehnologies}>
                         Tehnologies: 
                         <img src="/devicon_html5 (1).svg" alt="nextjs icon" /> HTML5
