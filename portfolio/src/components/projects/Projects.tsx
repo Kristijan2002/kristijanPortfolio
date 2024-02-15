ee5a2703a33677ff3a329e2a74d430a8339b8d9e
Kristijan20 <kikomaksimovski2002@gmail.com>
2024-02-15 14:55:35 +0100

    .

--- portfolio/src/components/projects/Projects.tsx
+++ portfolio/src/components/projects/Projects.tsx
@@ -13,7 +13,7 @@ const Projects: React.FC = () => {
                     <img src="/breathe.mk.svg" alt="breathe mk" />
                 </div>
                 <div className={style.desc}>
-                    {/* <p><b>BREATHE MK</b> - Welcome to our upcoming platform designed to empower the residents of Skopje to report instances of pollution in their city. Our website is currently under construction, but we're working diligently to create a seamless experience for you. Your feedback and inquiries are highly valued as we strive to make this platform as effective as possible. For any questions or to provide feedback, please feel free to reach out to Marija Salatic, our PO, at marija@revelapps.com.</p> */}
+                    <p><b>BREATHE MK</b> - Welcome to our upcoming platform designed to empower the residents of Skopje to report instances of pollution in their city. Our website is currently under construction, but we’re working diligently to create a seamless experience for you. Your feedback and inquiries are highly valued as we strive to make this platform as effective as possible. For any questions or to provide feedback, please feel free to reach out to Marija Salatic, our PO, at marija@revelapps.com.</p>
                     <p className={style.tehnologies}>
                         Tehnologies: 
                         <img src="/Laravel.svg" alt="laravel icon" /> Laravel
@@ -25,7 +25,7 @@ const Projects: React.FC = () => {
 
             <div className={style.project}>
                 <div className={style.desc}>
-                    {/* <p className={style.alignRight}><b>KRIK WEBSITE</b> - Engineered a cutting-edge web application utilizing Next.js for the renowned non-governmental organization, Crik. This platform optimizes the presentation of the organization's initiatives, fostering international support. While the application is still under development, stay tuned for its public release!</p> */}
+                <p className={style.alignRight}><b>KRIK WEBSITE</b> - Engineered a cutting-edge web application utilizing Next.js for the renowned non-governmental organization, Crik. This platform optimizes the presentation of the organizations initiatives, fostering international support. While the application is still under development, stay tuned for its public release!</p>
                     <p className={style.tehnologies}>
                         Tehnologies: 
                         <img src="/devicon_nextjs (1).svg" alt="nextjs icon" /> NextJs
@@ -43,7 +43,7 @@ const Projects: React.FC = () => {
                     <img src="/Marinov Design.svg" alt="marinov design" />
                 </div>
                 <div className={style.desc}>
-                    {/* <p><b>MARINOV DESIGN</b> - Engineered an E-Commerce web application tailored for Marinov Design, a thriving business in Ohrid. Leveraging Next.js, the platform amplifies online visibility, ensures secure transactions, and fuels revenue growth.</p> */}
+                <p className={style.alignRight}><b>MARINOV DESIGN</b> - Engineered an E-Commerce web application tailored for Marinov Design, a thriving business in Ohrid. Leveraging Next.js, the platform amplifies online visibility, ensures secure transactions, and fuels revenue growth.</p>
                     <p className={style.tehnologies}>
                         Tehnologies: 
                         <img src="/devicon_nextjs (1).svg" alt="nextjs icon" /> NextJs
