54729ff2fdb855ef83d8e9f39efc183e63451af9
unknown <kikom@LAP-005.melontech.com>
2024-03-02 20:41:56 +0100

    ui refinements

---
+++ portfolio/projects.ts
@@ -0,0 +1,53 @@
+const projects = [
+    {  
+      id: "1",
+      title: "BREATHE MK",
+      description: "Welcome to our upcoming platform designed to empower the residents of Skopje to report instances of pollution in their city. Our website is currently under construction, but we’re working diligently to create a seamless experience for you. Your feedback and inquiries are highly valued as we strive to make this platform as effective as possible. For any questions or to provide feedback, please feel free to reach out to Marija Salatic, our PO, at marija@revelapps.com.",
+      imgPath: "/breathe.mk.svg",
+      hostLink: "https://breathe.ddns.net/",
+      gitLink: "",
+      technologies: ["Laravel", "SASS", "JavaScript"],
+      technologyImages: ["/Laravel.svg", "/vscode-icons_file-type-css (1).svg", "/logos_javascript (1).svg"]
+    },
+    {
+      id: "2",
+      title: "KRIK WEBSITE",
+      description: "Engineered a cutting-edge web application utilizing Next.js for the renowned non-governmental organization, Crik. This platform optimizes the presentation of the organization's initiatives, fostering international support. While the application is still under development, stay tuned for its public release!",
+      imgPath: "/krik.svg",
+      hostLink: "https://krik.vercel.app/",
+      gitLink: "https://github.com/Kristijan2002/Krik",
+      technologies: ["Next.js", "TypeScript", "SASS"],
+      technologyImages: ["/devicon_nextjs (1).svg", "/vscode-icons_file-type-typescript-official (1).svg", "/vscode-icons_file-type-css (1).svg"]
+    },
+    {
+        title: "MARINOV DESIGN",
+        description:"Engineered an E-Commerce web application tailored for Marinov Design, a thriving business in Ohrid. Leveraging Next.js, the platform amplifies online visibility, ensures secure transactions, and fuels revenue growth.",
+        imgPath: "/Marinov Design.svg",
+        hostLink: "https://github.com/Kristijan2002/marinov",
+        gitLink: "https://github.com/Kristijan2002/marinov",
+        technologies: ["Next.js", "TypeScript", "CSS"],
+        technologyImages: ["/devicon_nextjs (1).svg", "/vscode-icons_file-type-typescript-official (1).svg", "/vscode-icons_file-type-css (1).svg"
+        ]
+      },
+      {
+        title: "STREETARTIST",
+        description:"Welcome to our innovative online platform tailored specifically for street artists. Our platform offers a comprehensive solution for managing earnings and provides a dedicated digital canvas to showcase your exceptional artworks to the world. Experience the convenience and power of this unique platform, meticulously designed for seamless use on mobile devices.",
+        imgPath: "/artiststreet.svg",
+        gitLink: "https://github.com/Kristijan2002/BrainsterLabs/tree/Project-02-StreetARTists",
+        liveDemoLink: "https://streetartist.netlify.app/",
+        technologies: ["HTML5", "CSS", "JavaScript"],
+        technologyImages: ["/devicon_html5 (1).svg", "/vscode-icons_file-type-css (1).svg", "/logos_javascript (1).svg"]
+      },
+      {
+        title: "BRAINSTERLABS",
+        description:"Discover the Brainster Labs website - an online platform where students from all Brainster academies can proudly showcase their projects. Experience this responsive website, meticulously crafted for seamless viewing on mobile devices, tablets, laptops, and desktop computers.",
+        imgPath: "/brainsterlabs.svg",
+        gitLink: "https://github.com/Kristijan2002/BrainsterLabs/tree/Project-02-StreetARTists",
+        liveDemoLink: "https://streetartist.netlify.app/",
+        technologies: ["HTML5", "CSS", "JavaScript"],
+        technologyImages: ["/devicon_html5 (1).svg", "/vscode-icons_file-type-css (1).svg", "/logos_javascript (1).svg"]
+      }
+  ];
+  
+  export default projects;
+  
